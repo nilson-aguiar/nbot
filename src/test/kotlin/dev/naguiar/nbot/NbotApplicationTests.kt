d7a807655f9ec7f02f20ea662c7dcf8893ebb183
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-04-26 07:40:49 +0200

    disable app test

--- src/test/kotlin/dev/naguiar/nbot/NbotApplicationTests.kt
+++ src/test/kotlin/dev/naguiar/nbot/NbotApplicationTests.kt
@@ -1,9 +1,11 @@
 package dev.naguiar.nbot
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled("Temporarily disabled, until app is ready for version 1")
 class NbotApplicationTests {
     @Test
     fun contextLoads() {
