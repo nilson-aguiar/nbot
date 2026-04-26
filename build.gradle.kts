9228ca366cd326664ed0d0fad47ce7f9ef4bd128
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-04-26 09:31:25 +0200

    chore: add thymeleaf dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ extra["springCloudVersion"] = "2025.0.2"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
