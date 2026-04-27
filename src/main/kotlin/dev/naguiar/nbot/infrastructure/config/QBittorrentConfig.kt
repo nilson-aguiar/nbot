5f2423f1a6eaa5d3c5d6a139fb9aa531130a8c58
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-04-27 06:12:33 +0200

    fix(torrent): use HttpComponentsClientHttpRequestFactory to disable chunking

--- src/main/kotlin/dev/naguiar/nbot/infrastructure/config/QBittorrentConfig.kt
+++ src/main/kotlin/dev/naguiar/nbot/infrastructure/config/QBittorrentConfig.kt
@@ -4,7 +4,7 @@ import dev.naguiar.nbot.tools.torrent.QBittorrentApi
 import dev.naguiar.nbot.tools.torrent.QBittorrentProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
@@ -13,10 +13,7 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory
 class QBittorrentConfig {
     @Bean
     fun qBittorrentApi(properties: QBittorrentProperties): QBittorrentApi {
-        val requestFactory = SimpleClientHttpRequestFactory().apply {
-            @Suppress("DEPRECATION")
-            setBufferRequestBody(true)
-        }
+        val requestFactory = HttpComponentsClientHttpRequestFactory()
         val client = RestClient.builder()
             .baseUrl(properties.url)
             .requestFactory(requestFactory)
