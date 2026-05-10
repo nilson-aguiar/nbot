ce404b9610591f798f3e673ce671b5c0964b059c
Nilson Aguiar <5804096+nilson-aguiar@users.noreply.github.com>
2026-05-10 08:25:49 +0200

    feat: add default account id configuration

--- src/main/kotlin/dev/naguiar/nbot/budget/infrastructure/config/ActualBudgetProperties.kt
+++ src/main/kotlin/dev/naguiar/nbot/budget/infrastructure/config/ActualBudgetProperties.kt
@@ -7,6 +7,7 @@ data class ActualBudgetProperties(
     val url: String = "http://localhost:5007",
     val apiKey: String = "",
     val syncId: String = "",
+    val defaultAccountId: String = "",
     val internalAccounts: List<String> = listOf(
         "Main account", "Joint account", "My son's account", "Month balance", "Extras",
         "Apolo", "Nilson", "Férias", "Poupança", "Mês seguinte", "Renata"
